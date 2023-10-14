8738799a33db7bbb0316e20cf1ca0c313275c71c
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-10-14 12:33:57 +0100

    app-delete-comented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,6 @@ import { PhoneBook } from './App.styled';
 
 const CONTACTS = 'contacts';
 
-// const oldContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
 export const App = () => {
   const [contacts, setContacts] = useState(() => getInitContacts() || []);
   const [filter, setFilter] = useState('');
@@ -73,74 +66,3 @@ export const App = () => {
     </PhoneBook>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contactsFromLocal = localStorage.getItem(CONTACTS);
-//     if (contactsFromLocal) {
-//       this.setState({
-//         contacts: JSON.parse(contactsFromLocal),
-//       });
-//     }
-//   }
-
-//   componentDidUpdate() {
-//     const contactForLocal = JSON.stringify(this.state.contacts);
-//     localStorage.setItem(CONTACTS, contactForLocal);
-//   }
-
-//   addContact = dataByForm => {
-// const alreadyExist = this.state.contacts.some(
-//   el => el.name.toLowerCase() === dataByForm.name.toLowerCase()
-// );
-// if (alreadyExist)
-//   return alert(`${dataByForm.name} is already in contacts.`);
-
-// const newContact = { id: nanoid(), ...dataByForm };
-// this.setState(prev => ({
-//   contacts: [newContact, ...prev.contacts],
-// }));
-//   };
-
-//   deleteContact = id => {
-// this.setState(prev => ({
-//   contacts: prev.contacts.filter(el => el.id !== id),
-// }));
-//   };
-
-//   getFilteredContacts = () => {
-// const { contacts, filter } = this.state;
-// return contacts.filter(el =>
-//   el.name.toLowerCase().includes(filter.toLowerCase())
-// );
-//   };
-
-//   handleFilter = ({ target: { value } }) => {
-//     this.setState({
-//       filter: value,
-//     });
-//   };
-
-//   render() {
-//     const filteredContacts = this.getFilteredContacts();
-
-//     return (
-//       <PhoneBook>
-//         <h1>Phonebook</h1>
-//         <ContactForm addContact={this.addContact} />
-
-//         <h2>Contacts</h2>
-//         <Filter handleFilter={this.handleFilter} />
-//         <ContactList
-//           contacts={filteredContacts}
-//           deleteContact={this.deleteContact}
-//         />
-//       </PhoneBook>
-//     );
-//   }
-// }
